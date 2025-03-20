79ddc5e32c947a1f7fe86e98badbfc4b3e83e8a8
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-03-20 11:21:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -6,28 +6,53 @@ using namespace std;
 
 // } Driver Code Ends
 
-//User function template for C++
-
-class Solution{   
-public:
-    bool isSubsetSum(vector<int>arr, int sum){
+class Solution {
+  public:
+  
+    // bool f(int ind, vector<int>& arr, int sum, vector<vector<int>> &dp){
+    //     if(sum == 0) return true;
+    //     if(ind == 0) return (arr[0] == sum);
+    //     if(dp[ind][sum] != -1) return dp[ind][sum];
+    //     bool notTake = f(ind-1, arr, sum, dp);
+    //     bool take = false;
+    //     take = f(ind - 1, arr, sum - arr[ind], dp);
+    //     return dp[ind][sum] = take | notTake;
+    // }
+    
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
         int n = arr.size();
-        vector<bool> prev(sum+1, 0), curr(sum+1, 0);
-        prev[0] = curr[0] = true;
+        vector<bool> prev(sum+1, 0), cur(sum+1, 0);
+        prev[0] = cur[0] = true;
         prev[arr[0]] = true;
-        for(int i=1; i<n; i++){
+        for(int ind = 1; ind<n; ind++){
             for(int k=1; k<=sum; k++){
                 bool notTake = prev[k];
                 bool take = false;
-                if(arr[i] <= k) take = prev[k-arr[i]];
-                curr[k] = take | notTake;
+                if(arr[ind] <= k) take = prev[k - arr[ind]];
+                cur[k] = take | notTake;
             }
-            prev = curr;
+            prev = cur;
         }
         return prev[sum];
     }
+    //     vector<vector<bool>> dp(n+1, vector<bool>(sum+1, 0));
+    //     for(int i=0; i<n; i++) dp[i][0] = true;
+    //     dp[0][arr[0]] = true;
+    //     for(int ind = 1; ind<n; ind++){
+    //         for(int k = 1; k<=sum; k++){
+    //             bool notTake = dp[ind-1][k];
+    //             bool take = false;
+    //             if(arr[ind] <= k)take = dp[ind-1][k - arr[ind]];
+    //             dp[ind][k] = notTake | take;
+    //         }
+    //     }
+    //     return dp[n-1][sum];
+    // }
+    //     vector<vector<int>> dp(n+1, vector<int> (sum+1, -1));
+    //     return f(n-1, arr, sum, dp);
+    // }
 };
-
 
 
 //{ Driver Code Starts.
