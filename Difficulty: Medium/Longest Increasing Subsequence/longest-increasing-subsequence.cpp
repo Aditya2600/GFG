7c24ddbf9a0dfc5534ca54c102877a6ff42c6d35
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-05-28 08:32:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -1,51 +1,19 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-
-
-class Solution
-{
-    public:
-    //Function to find length of longest increasing subsequence.
-    int longestSubsequence(int n, int a[])
-    {
-       vector<int> temp;
-       temp.push_back(a[0]);
-       for(int i=1; i<n; i++){
-           if(a[i]>temp.back()){
-               temp.push_back(a[i]);
-           }
-           else{
-               auto it = lower_bound(temp.begin(), temp.end(), a[i]);
-               int ind = it - temp.begin();
-               temp[ind] = a[i];
-           }
-       }
-       return temp.size();
+class Solution {
+  public:
+    int lis(vector<int>& arr) {
+        // code here
+        vector<int> temp;
+        int n = arr.size();
+        temp.push_back(arr[0]);
+        for(int i=1; i<n; i++){
+            if(temp.back() < arr[i]){
+                temp.push_back(arr[i]);
+            }
+            else{
+                int ind = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+                temp[ind] = arr[i];
+            }
+        }
+        return temp.size();
     }
 };
-
-//{ Driver Code Starts.
-int main()
-{
-    //taking total testcases
-    int t,n;
-    cin>>t;
-    while(t--)
-    {
-        //taking size of array
-        cin>>n;
-        int a[n];
-        
-        //inserting elements to the array
-        for(int i=0;i<n;i++)
-            cin>>a[i];
-        Solution ob;
-        //calling method longestSubsequence()
-        cout << ob.longestSubsequence(n, a) << endl;
-    }
-}
-
-// } Driver Code Ends
