5ec550c2ba40a6e477010484ece39f0129c9d3b5
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-02 23:29:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.cpp
+++ Difficulty: Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.cpp
@@ -2,37 +2,23 @@ class Solution {
   public:
     int countPairs(vector<vector<int>> &mat1, vector<vector<int>> &mat2, int x) {
         // code here
+        unordered_set<int> elements;  // to store elements of mat2
         int n = mat1.size();
-        vector<int> v1, v2;
         
-        // Flatten both matrices
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                v1.push_back(mat1[i][j]);
-                v2.push_back(mat2[i][j]);
-            }
-        }
-
-        // Sort not needed if input strictly follows conditions
-        // But good for safety or messy inputs
-        sort(v1.begin(), v1.end());
-        sort(v2.begin(), v2.end());
-
-        int i = 0, j = v2.size() - 1;
+        // Insert all elements of mat2 into the set
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                elements.insert(mat2[i][j]);
+        
         int count = 0;
 
-        while (i < v1.size() && j >= 0) {
-            int sum = v1[i] + v2[j];
-            if (sum == x) {
-                ++count;
-                ++i;
-                --j;
-            } else if (sum < x) {
-                ++i;
-            } else {
-                --j;
+        // Check for complement of each element in mat1
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j) {
+                int a = mat1[i][j];
+                if (elements.find(x - a) != elements.end())
+                    count++;
             }
-        }
 
         return count;
     }
