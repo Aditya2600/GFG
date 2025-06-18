d2c312ab6d3fb3a295429fc6fa0a3da3d9506d06
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-06-18 18:40:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Piles/coin-piles.cpp
+++ Difficulty: Medium/Coin Piles/coin-piles.cpp
@@ -1,38 +1,36 @@
 class Solution {
-public:
+  public:
     int minimumCoins(vector<int>& arr, int k) {
-        sort(arr.begin(), arr.end());
         int n = arr.size();
+        sort(arr.begin(), arr.end());
 
-        vector<int> prefix(n + 1, 0);
-        for (int i = 0; i < n; ++i) {
-            prefix[i + 1] = prefix[i] + arr[i];
+        vector<long long> prefix(n);
+        prefix[0] = arr[0];
+        for(int i = 1; i < n; i++) {
+            prefix[i] = prefix[i-1] + arr[i];
         }
 
-        int ans = INT_MAX;
+        long long totalSum = prefix[n-1];
+        long long ans = LLONG_MAX;
 
-        for (int i = 0; i < n; ++i) {
-            int remLeft = prefix[i];
-            int upper = upperBound(arr, arr[i] + k);
-            int remRight = prefix[n] - prefix[upper] - (n - upper) * (arr[i] + k);
-            ans = min(ans, remLeft + remRight);
+        for(int i = 0; i < n; i++) {
+            // arr[i] is considered as the minimum
+            int maxAllowed = arr[i] + k;
+            
+            // Find the last position where arr[pos] <= maxAllowed
+            int pos = upper_bound(arr.begin(), arr.end(), maxAllowed) - arr.begin();
+
+            // Remove all piles before i completely (i piles)
+            long long removeLeft = (i > 0) ? prefix[i-1] : 0;
+
+            // Remove excess coins from arr[pos] to arr[n-1]
+            long long keepRightPiles = (n - pos) * 1LL * maxAllowed;
+            long long sumRight = (pos < n) ? (prefix[n-1] - prefix[pos - 1]) : 0;
+            long long removeRight = (pos < n) ? (sumRight - keepRightPiles) : 0;
+
+            ans = min(ans, removeLeft + removeRight);
         }
 
-        return ans;
-    }
-
-private:
-    int upperBound(vector<int>& arr, int v) {
-        int lo = 0, hi = arr.size() - 1, ans = arr.size();
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-            if (arr[mid] <= v) {
-                lo = mid + 1;
-            } else {
-                ans = mid;
-                hi = mid - 1;
-            }
-        }
-        return ans;
+        return (int)ans;
     }
 };
