5b9fc90eb5e8ca8f2ef361b5943400c5419e4b98
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-07-06 20:11:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Sum Combination/maximum-sum-combination.cpp
+++ Difficulty: Medium/Maximum Sum Combination/maximum-sum-combination.cpp
@@ -1,67 +1,45 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
+#include <vector>
+#include <queue>
+#include <set>
+#include <tuple>
+#include <algorithm>
+
 using namespace std;
 
-
-// } Driver Code Ends
 class Solution {
   public:
-    vector<int> maxCombinations(int N, int K, vector<int> &A, vector<int> &B) {
-        vector<int> ans;
-        // for(int i=0; i<N; i++){
-        //     for(int j=0; j<N; j++){
-        //         ans.push_back(A[i] + B[j]);
-        //     }
-        // }
-        // sort(ans.begin(),ans.end(), greater<int>());
-        // vector<int> result(ans.begin(), ans.begin()+K);
-        // return result;
-        sort(A.begin(), A.end());
-        sort(B.begin(), B.end());
-        priority_queue<pair<int,pair<int,int>>> pq;
-        set<pair<int, int>> s;
-        pq.push({A.back() + B.back(), {N-1,N-1}});
-        while(K--){
-            auto curr = pq.top();
-            int sum = curr.first;
-            int i = curr.second.first;
-            int j = curr.second.second;
-            ans.push_back(sum);
-            pq.pop();
-            if(s.find({i-1,j}) == s.end()){
-                pq.push({A[i-1] + B[j], {i-1, j}});
-                s.insert({i-1, j});
+    vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
+        int n = a.size();
+        sort(a.begin(), a.end(), greater<int>());
+        sort(b.begin(), b.end(), greater<int>());
+
+        priority_queue<tuple<int, int, int>> maxHeap;
+        set<pair<int, int>> seen;
+
+        maxHeap.push(make_tuple(a[0] + b[0], 0, 0));
+        seen.insert({0, 0});
+
+        vector<int> result;
+
+        while (k-- && !maxHeap.empty()) {
+            auto top = maxHeap.top();
+            maxHeap.pop();
+            int sum = get<0>(top);
+            int i = get<1>(top);
+            int j = get<2>(top);
+            result.push_back(sum);
+
+            if (i + 1 < n && seen.find({i + 1, j}) == seen.end()) {
+                maxHeap.push(make_tuple(a[i + 1] + b[j], i + 1, j));
+                seen.insert({i + 1, j});
             }
-            if(s.find({i,j-1}) == s.end()){
-                pq.push({A[i] + B[j-1], {i, j-1}});
-                s.insert({i, j-1});
+
+            if (j + 1 < n && seen.find({i, j + 1}) == seen.end()) {
+                maxHeap.push(make_tuple(a[i] + b[j + 1], i, j + 1));
+                seen.insert({i, j + 1});
             }
         }
-        return ans;
+
+        return result;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N, K;
-        cin >> N >> K;
-
-        vector<int> A(N), B(N);
-        for (int i = 0; i < N; i++) {
-            cin >> A[i];
-        }
-        for (int i = 0; i < N; i++) {
-            cin >> B[i];
-        }
-        Solution obj;
-        vector<int> ans = obj.maxCombinations(N, K, A, B);
-        for (auto &it : ans) cout << it << ' ';
-        cout << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
