c303ce0d9abe36916ee8b444fb9997aa0f078a58
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-18 08:24:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Deletions/minimum-deletions.cpp
+++ Difficulty: Medium/Minimum Deletions/minimum-deletions.cpp
@@ -1,23 +1,28 @@
 class Solution {
-  public:
+public:
     int minDeletions(string s) {
-        // code here
-        int n = s.length();
-        string t = s;
-        reverse(t.begin(), t.end());
+        int n = s.size();
+        if (n <= 1) return 0;
 
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        // dp[i][j] = LPS length in s[i..j]
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        // Find LCS of s and its reverse => LPS
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
-                if(s[i - 1] == t[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        // substrings of length 1
+        for (int i = 0; i < n; ++i) dp[i][i] = 1;
+
+        // build for increasing lengths
+        for (int len = 2; len <= n; ++len) {
+            for (int i = 0; i + len - 1 < n; ++i) {
+                int j = i + len - 1;
+                if (s[i] == s[j]) {
+                    dp[i][j] = 2 + (i+1 <= j-1 ? dp[i+1][j-1] : 0);
+                } else {
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+                }
             }
         }
 
-        return n - dp[n][n];
+        int lps = dp[0][n-1];
+        return n - lps;
     }
 };
