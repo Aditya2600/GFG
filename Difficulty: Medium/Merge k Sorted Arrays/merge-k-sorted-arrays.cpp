f03f1294670d5a1e62040c1b3f7c7a50b553547a
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-10-21 16:05:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.cpp
@@ -1,89 +1,27 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include<bits/stdc++.h>
-#define N 105
-using namespace std;
-void printArray(vector<int> arr, int size)
-{
-for (int i=0; i < size; i++)
-	cout << arr[i] << " ";
-}
-
-
-// } Driver Code Ends
-//User function Template for C++
-class data{
-    public:
-    int val, apos, vpos;
-    data(int v, int ap, int vp){
-        val = v;
-        apos = ap;
-        vpos = vp;
-    }
-};
-
-struct mycomp{
-    bool operator()(data &d1, data &d2){
-        return d1.val > d2.val;
-    }
-};
-
-class Solution
-{
-    public:
-    //Function to merge k sorted arrays.
-    vector<int> mergeKArrays(vector<vector<int>> arr, int K)
-    {
-        //code here
-        vector<int> ans;
-        priority_queue<data, vector<data>, mycomp> pq;
-        for(int i=0; i<K; i++){
-            data d(arr[i][0], i, 0);
-            pq.push(d);
+class Solution {
+  public:
+    vector<int> mergeArrays(vector<vector<int>> &mat) {
+        // Code here
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> idx(n, 0);
+        for(int i=0; i<n; i++){
+            pq.push({mat[i][0], i});
+            idx[i] = 1;
         }
+        vector<int> ans;
         while(!pq.empty()){
-            data curr = pq.top();
+            auto [val, r] = pq.top();
             pq.pop();
-            ans.push_back(curr.val);
-            int ap = curr.apos, vp = curr.vpos;
-            if(vp + 1 < arr[ap].size()){
-                data d(arr[ap][vp+1], ap, vp+1);
-                pq.push(d);
+            ans.push_back(val);
+            
+            if(idx[r] < mat[r].size()){
+                pq.push({mat[r][idx[r]], r});
+                idx[r]++;
             }
         }
         return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--){
-	    int k;
-	    cin>>k;
-	    vector<vector<int>> arr(k, vector<int> (k, 0));
-	    for(int i=0; i<k; i++){
-	        for(int j=0; j<k; j++)
-	        {
-	            cin>>arr[i][j];
-	        }
-	    }
-	    Solution obj;
-    	vector<int> output = obj.mergeKArrays(arr, k);
-    	printArray(output, k*k);
-    	cout<<endl;
-    }
-	return 0;
-}
-
-
-
-
-
-
-// } Driver Code Ends
