0d3efaac1ecbcab83e62d39bfb5e61dd7c7b701c
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-12-01 16:33:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -1,34 +1,73 @@
+// class Solution {
+//     private:
+//         void dfs(int node, vector<int>& vis, vector<vector<int>>& adj, stack<int>& st){
+//             vis[node] = 1;
+//             for(auto it : adj[node]){
+//                 if(!vis[it]){
+//                     dfs(it, vis, adj, st);
+//                 }
+//             }
+//             st.push(node);
+//         }
+//   public:
+//     vector<int> topoSort(int V, vector<vector<int>>& edges) {
+//         vector<int> ans;
+//         vector<int> vis(V, 0);
+//         vector<vector<int>> adj(V);
+//         for(int i=0;i<edges.size();i++){
+//             int u = edges[i][0];
+//             int v = edges[i][1];
+//             adj[u].push_back(v);
+//         }
+//         stack<int> st;
+//         for(int i=0; i<V; i++){
+//             if(!vis[i]){
+//                 dfs(i, vis, adj, st);
+//             }
+//         }
+//         while(!st.empty()){
+//             ans.push_back(st.top());
+//             st.pop();
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
-    private:
-        void dfs(int node, vector<int>& vis, vector<vector<int>>& adj, stack<int>& st){
-            vis[node] = 1;
-            for(auto it : adj[node]){
-                if(!vis[it]){
-                    dfs(it, vis, adj, st);
-                }
-            }
-            st.push(node);
-        }
   public:
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
-        vector<int> ans;
-        vector<int> vis(V, 0);
         vector<vector<int>> adj(V);
-        for(int i=0;i<edges.size();i++){
+        vector<int> indegree(V);
+        for(int i=0; i<edges.size(); i++){
             int u = edges[i][0];
             int v = edges[i][1];
             adj[u].push_back(v);
         }
-        stack<int> st;
         for(int i=0; i<V; i++){
-            if(!vis[i]){
-                dfs(i, vis, adj, st);
+            for(auto it : adj[i]){
+                indegree[it]++;
             }
         }
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
+        queue<int> qu;
+        for(int i=0; i<V; i++){
+            if(indegree[i] == 0){
+                qu.push(i);
+            }
         }
-        return ans;
+        
+        vector<int> topo;
+        while(!qu.empty()){
+            int node = qu.front();
+            qu.pop();
+            topo.push_back(node);
+            
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    qu.push(it);
+                }
+            }
+        }
+        return topo;
     }
 };
