76683de7e3996cafa9c597477493d384e392f6d9
Aditya <126133127+Aditya2600@users.noreply.github.com>
2025-12-06 18:15:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Word Ladder II/word-ladder-ii.cpp
+++ Difficulty: Hard/Word Ladder II/word-ladder-ii.cpp
@@ -1,46 +1,40 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function Template for C++
+// User function Template for C++
 
 class Solution {
-public:
-    vector<vector<string>> findSequences(string beginWord, string endWord, vector<string>& wordList) {
-        
+  public:
+    vector<vector<string>> findSequences(string beginWord, string endWord,
+                                         vector<string>& wordList) {
+        // code here
         unordered_set<string> st(wordList.begin(), wordList.end());
+        if(st.count(endWord) == 0) return {};
         queue<vector<string>> q;
         q.push({beginWord});
+        int level = 1;
         vector<string> usedOnLevel;
-        usedOnLevel.push_back(beginWord);
-        int level = 0;
         vector<vector<string>> ans;
-        while (!q.empty()) {
+        while(!q.empty()){
             vector<string> vec = q.front();
             q.pop();
-
-            if (vec.size() > level) {
+            if(vec.size() > level){
                 level++;
-                for (auto it : usedOnLevel) {
+                for(auto it : usedOnLevel){
                     st.erase(it);
                 }
-                usedOnLevel.clear();
             }
             string word = vec.back();
-            if (word == endWord) {
-                if (ans.size() == 0) {
+            if(word == endWord){
+                if(ans.size() == 0){
                     ans.push_back(vec);
-                } else if (ans[0].size() == vec.size()) {
+                }
+                else if(ans[0].size() == vec.size()){
                     ans.push_back(vec);
                 }
             }
-            for (int i = 0; i < word.size(); i++) {
+            for(int i=0; i<word.size(); i++){
                 char original = word[i];
-                for (char ch = 'a'; ch <= 'z'; ch++) {
+                for(char ch = 'a'; ch <= 'z'; ch++){
                     word[i] = ch;
-                    if (st.count(word) > 0) {
+                    if(st.count(word) > 0){
                         vec.push_back(word);
                         q.push(vec);
                         usedOnLevel.push_back(word);
@@ -53,47 +47,3 @@ public:
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-bool comp(vector<string> a, vector<string> b)
-{
-    string x = "", y = "";
-    for(string i: a)
-        x += i;
-    for(string i: b)
-        y += i;
-    
-    return x<y;
-}
-
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n;
-		cin >> n;
-		vector<string>wordList(n);
-		for(int i = 0; i < n; i++)cin >> wordList[i];
-		string startWord, targetWord;
-		cin >> startWord >> targetWord;
-		Solution obj;
-		vector<vector<string>> ans = obj.findSequences(startWord, targetWord, wordList);
-		if(ans.size()==0)
-		    cout<<-1<<endl;
-		else
-		{
-		    sort(ans.begin(), ans.end(), comp);
-            for(int i=0; i<ans.size(); i++)
-            {
-                for(int j=0; j<ans[i].size(); j++)
-                {
-                    cout<<ans[i][j]<<" ";
-                }
-                cout<<endl;
-            }
-		}
-	}
-	return 0;
-}
-// } Driver Code Ends
